be6b6ec03ab7ce9fdd67031fc41c50c05f560a64
Paul MARCOMBES <paul.marcombes@nickel.eu>
2022-09-02 16:14:22 +0200

    render_string improved

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,3 +1,4 @@
+import os
 import argparse
 
 import requests
@@ -15,34 +16,24 @@ BQ = google.cloud.bigquery.Client()
 # JS_LIBS_BUCKET = STORAGE.get_bucket(CONF['js_libs_bucket'])
 
 
-
-parser = argparse.ArgumentParser(description='Deploy BigQuery asset among [dataset, table, bigfunction]')
-parser.add_argument('environment', choices=ENVIRONMENTS)
-parser.add_argument('asset_type', choices=['dataset', 'table', 'bigfunction'])
-parser.add_argument('--name')
-parser.add_argument('--region')
-args = parser.parse_args()
-assert args.asset_type == 'dataset' or args.name, 'a name argument must be given if asset_type != dataset. Run `python scripts/deploy_bigquery.py --help`'
-
-
-for region in [args.region] if args.region else CONF['bigquery_regions']:
+def deploy(asset_type, name, region, environment):
     CONF['region__lowercase_and_wo_hyphens'] = region.lower().replace('-', '_')
 
-    dataset = {**[dataset for dataset in CONF['datasets'] if dataset['env'] == args.environment][0]}
+    dataset = {**[dataset for dataset in CONF['datasets'] if dataset['env'] == environment][0]}
     dataset['name'] = dataset['name'].format(**CONF)
     dataset['description'] = dataset['description'].format(**CONF)
     full_dataset_name = f"{CONF['project']}.{dataset['name']}"
 
     conf = {}
-    template_file = f'scripts/templates/{args.asset_type}.sql'
-    if args.asset_type == 'table':
-        conf = CONF['tables'][args.name]
-    elif args.asset_type == 'bigfunction':
-        filename = f'bigfunctions/{args.name}.yaml'
+    template_file = f'scripts/templates/{asset_type}.sql'
+    if asset_type == 'table':
+        conf = CONF['tables'][name]
+    elif asset_type == 'bigfunction':
+        filename = f'bigfunctions/{name}.yaml'
         conf = yaml.safe_load(open(filename, encoding='utf-8').read().replace('{BIGFUNCTIONS_DATASET}', full_dataset_name))
-        conf['name'] = args.name
+        conf['name'] = name
         conf['filename'] = filename
-        conf['description'] = conf['description'] + f'\n\nSee full documentation --> "{CONF["doc_base_url"]}#{args.name}"'
+        conf['description'] = conf['description'] + f'\n\nSee full documentation --> "{CONF["doc_base_url"]}#{name}"'
         if 'template' in conf:
             conf['code'] += f'''
                 set output = to_json(struct(
@@ -67,7 +58,7 @@ for region in [args.region] if args.region else CONF['bigquery_regions']:
 
     template = jinja2.Template(open(template_file, encoding='utf-8').read())
     query = template.render(
-        environment=args.environment,
+        environment=environment,
         region=region,
         dataset=dataset,
         **CONF,
@@ -80,9 +71,30 @@ for region in [args.region] if args.region else CONF['bigquery_regions']:
     #     blob = google.cloud.storage.Blob(library['filename'], JS_LIBS_BUCKET)
     #     blob.upload_from_string(js)
 
-    print(query)
     BQ.query(query, location=region).result()
-    print('successfully created', args.asset_type, 'for region', region, 'and environment', args.environment)
+    print('successfully created', asset_type, name, 'for region', region, 'and environment', environment)
 
 
+
+
+
+parser = argparse.ArgumentParser(description='Deploy BigQuery asset among [dataset, table, bigfunction]')
+parser.add_argument('environment', choices=ENVIRONMENTS)
+parser.add_argument('asset_type', choices=['dataset', 'table', 'bigfunction'])
+parser.add_argument('--name')
+parser.add_argument('--region')
+args = parser.parse_args()
+
+
+regions = [args.region] if args.region else CONF['bigquery_regions']
+if args.asset_type == 'dataset':
+    names = ['']
+elif args.asset_type == 'table':
+    names = [args.name] if args.name else CONF['tables'].keys()
+elif args.asset_type == 'bigfunction':
+    names = [args.name] if args.name else [f.replace('.yaml', '') for f in os.listdir('bigfunctions') if f.endswith('.yaml')]
+
+for name in names:
+    for region in regions:
+        deploy(args.asset_type, name, region, args.environment)
 
