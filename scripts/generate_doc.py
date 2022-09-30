2141152c463f1ce1e8c8fd39feb46a33b05887bb
Paul MARCOMBES <paul.marcombes@nickel.eu>
2022-10-01 00:10:20 +0200

    doc refacto 12

--- scripts/generate_doc.py
+++ scripts/generate_doc.py
@@ -4,20 +4,20 @@ import yaml
 import jinja2
 
 CONF = yaml.safe_load(open(f'mkdocs.yml', encoding='utf-8').read())
-BIGFUNCTIONS = [
-    {
-        **yaml.safe_load(open(f'bigfunctions/{f}', encoding='utf-8').read()),
-        **{'name': f.replace('.yaml', '')},
-    }
-    for f in os.listdir('bigfunctions')
-]
 
 
 def generate_bigfunctions_category_page():
     template = f'scripts/templates/doc_reference.md'
+    bigfunctions = [
+        {
+            **yaml.safe_load(open(f'bigfunctions/{f}', encoding='utf-8').read()),
+            **{'name': f.replace('.yaml', '')},
+        }
+        for f in os.listdir('bigfunctions')
+    ]
     categories = CONF['bigfunctions_categories']
     for category in categories:
-        category['bigfunctions'] = [bigfunction for bigfunction in BIGFUNCTIONS if bigfunction['category'] == category['name']]
+        category['bigfunctions'] = [bigfunction for bigfunction in bigfunctions if bigfunction['category'] == category['name']]
     documentation = jinja2.Template(open(template, encoding='utf-8').read()).render(
         datasets=CONF['bigfunctions_datasets'],
         repo_url=CONF['repo_url'],
