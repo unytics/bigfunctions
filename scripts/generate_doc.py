404aeb3b2b15368a3011d26bb6723da6a3f8662f
Paul MARCOMBES <paul.marcombes@nickel.eu>
2022-09-30 23:51:27 +0200

    doc refacto 11

--- scripts/generate_doc.py
+++ scripts/generate_doc.py
@@ -4,9 +4,6 @@ import yaml
 import jinja2
 
 CONF = yaml.safe_load(open(f'mkdocs.yml', encoding='utf-8').read())
-
-REGIONS_TO_DISPLAY = ['EU', 'US', 'europe-west1', 'your-region2']
-REPO = 'https://github.com/unytics/bigfunctions'
 BIGFUNCTIONS = [
     {
         **yaml.safe_load(open(f'bigfunctions/{f}', encoding='utf-8').read()),
@@ -22,8 +19,8 @@ def generate_bigfunctions_category_page():
     for category in categories:
         category['bigfunctions'] = [bigfunction for bigfunction in BIGFUNCTIONS if bigfunction['category'] == category['name']]
     documentation = jinja2.Template(open(template, encoding='utf-8').read()).render(
-        regions=REGIONS_TO_DISPLAY,
-        repo=REPO,
+        regions=CONF['bigfunctions_regions'],
+        repo_url=CONF['repo_url'],
         categories=categories,
     )
     with open('site/content/reference.md', 'w', encoding='utf-8') as out:
