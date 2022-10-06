6007328e5b6444c249748d9b441e6c0c97ceaa15
Paul MARCOMBES <paul.marcombes@unytics.io>
2022-10-06 22:06:42 +0200

    minor

--- bigfun/cli.py
+++ bigfun/cli.py
@@ -23,9 +23,7 @@ def get_config_value(name):
 
     text, default = {
         'default_gcp_project':          ("Default GCP project where to deploy bigfunctions", "bigfunctions"),
-        'default_gcp_project_for_test': ("Default GCP project where to deploy bigfunctions for testing purposes", "bigfunctions"),
         'default_datasets':             ("Default dataset(s) where to deploy bigfunctions (comma separated is many)", "eu,us,asia_east1,asia_east2,asia_northeast1,asia_northeast2,asia_northeast3,asia_south1,asia_southeast1,australia_southeast1,europe_north1,europe_west1,europe_west2,europe_west3,europe_west4,europe_west6,northamerica_northeast1,southamerica_east1,us_central1,us_east1,us_east4,us_west1,us_west2"),
-        'default_datasets_for_test':    ("Default dataset(s) where to deploy bigfunctions for testing purposes (comma separated is many)", "test_eu,test_us,test_asia_east1,test_asia_east2,test_asia_northeast1,test_asia_northeast2,test_asia_northeast3,test_asia_south1,test_asia_southeast1,test_australia_southeast1,test_europe_north1,test_europe_west1,test_europe_west2,test_europe_west3,test_europe_west4,test_europe_west6,test_northamerica_northeast1,test_southamerica_east1,test_us_central1,test_us_east1,test_us_east4,test_us_west1,test_us_west2"),
     }[name]
     CONFIG[name] = click.prompt(text, default=default)
     with open(CONFIG_FILENAME, 'w', encoding='utf-8') as outfile:
