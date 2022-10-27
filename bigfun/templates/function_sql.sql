bf57f9faeecd5a6a39b6d8de019bd1947efa1538
unytics <paul.marcombes@unytics.io>
2022-10-27 23:07:30 +0200

    last_element

--- bigfun/templates/function_sql.sql
+++ bigfun/templates/function_sql.sql
@@ -1,5 +1,5 @@
 create or replace function {{ dataset }}.{{ name }}({% for argument in arguments %}`{{ argument.name }}` {{ argument.type}}{% if not loop.last %}, {% endif %}{% endfor %})
-returns {{ output.type }}
+{% if output.type != 'any type' %}returns {{ output.type }}{% endif %}
 as (
 {{ code }}
 )
