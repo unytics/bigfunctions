d65cebf4ffdeb44af23167c3100d62db4fbf34e7
Unytics <111615732+unytics@users.noreply.github.com>
2023-07-13 15:36:59 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='bigfun',
     version='0.1.0',
     packages=['bigfun'],
-    package_data={'': ['test_template.js']},
+    package_data={'': ['*']},
     include_package_data=True,
     install_requires=[
         'google-cloud-bigquery',
