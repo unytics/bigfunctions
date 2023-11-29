04d3a253a06e4a6a4bc2fa0103391c06db39ddf1
unytics <paul.marcombes@unytics.io>
2023-11-29 23:17:57 +0100

    load_table: return if no csv exists

--- bigfun/load_table.py
+++ bigfun/load_table.py
@@ -1,3 +1,4 @@
+import os
 
 import google.cloud.bigquery
 import yaml
@@ -9,9 +10,12 @@ DATA_FOLDER = 'data'
 
 def load_table(table):
     project, dataset, table_name = table.split('.')
+    filename = f'{DATA_FOLDER}/{table_name}.csv'
+    if not os.path.isfile(filename):
+        return
     bigquery = BigQuery(project)
     conf = yaml.safe_load(open(f'{DATA_FOLDER}/{table_name}.yaml', encoding='utf-8').read())
-    with open(f'{DATA_FOLDER}/{table_name}.csv', 'rb') as file:
+    with open(filename, 'rb') as file:
         bigquery.create_or_replace_destination_table(table, conf)
         job_config = google.cloud.bigquery.job.LoadJobConfig(source_format= 'CSV')
         bigquery.load_table_from_file(file, table, job_config=job_config).result()
